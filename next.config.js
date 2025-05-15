8c5cef601108c6df07d6dc3d5fc41b87ffc8484f
Your Name <youremail@domain.com>
2025-05-15 05:30:23 -0400

    Adj4

--- next.config.js
+++ next.config.js
@@ -15,39 +15,19 @@ const nextConfig = {
   webpack: (config, { isServer }) => {
     config.resolve.fallback = { 
       fs: false,
-      // Add other Node.js modules that need to be polyfilled
       net: false,
       tls: false,
       dns: false,
       'stripe': false
     };
-    // Handle ESM modules
+    
     config.resolve.extensionAlias = {
       '.js': ['.js', '.ts', '.tsx'],
     };
-
-    // Add support for private class fields
-    config.module = config.module || {};
-    config.module.rules = config.module.rules || [];
-    config.module.rules.push({
-      test: /\.(js|mjs|jsx|ts|tsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['next/babel'],
-          plugins: [
-            '@babel/plugin-proposal-private-methods',
-            '@babel/plugin-proposal-private-property-in-object',
-            '@babel/plugin-proposal-class-properties'
-          ]
-        }
-      }
-    });
 
     return config;
   },
   transpilePackages: ['debug', 'supports-color', 'undici', 'firebase'],
 };
 
-module.exports = withPWA(nextConfig); 
+module.exports = withPWA(nextConfig);
